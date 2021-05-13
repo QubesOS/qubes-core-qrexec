adb1301112ee8d72fa56a6d8ba77a0aff2cab4eb
Demi Marie Obenour <demi@invisiblethingslab.com>
2021-05-13 13:32:21 -0400

    Add unit tests for policy.EvalSimple
    To make sure it works properly.

--- qrexec/tests/qrexec_policy_daemon.py
+++ qrexec/tests/qrexec_policy_daemon.py
@@ -53,15 +53,32 @@ class TestPolicyDaemon:
 
         server.close()
 
-    async def send_data(self, server, path, data):
+    @pytest.fixture
+    async def qrexec_server(self, tmp_path, request):
+        log = unittest.mock.Mock()
+
+        qrexec_server = await asyncio.start_unix_server(
+            functools.partial(qrexec_policy_daemon.handle_qrexec_connection,
+                              log, Mock()),
+            path=str(tmp_path / "socket.qrexec"))
+
+        yield server
+
+        server.close()
+
+    async def send_data(self, server, path, data, qrexec=False):
         reader, writer = await asyncio.open_unix_connection(
-            str(path / "socket.d"))
+            str(path / "socket.qrexec" if qrexec else "socket.d"))
         writer.write(data)
+        if qrexec:
+            writer.close()
+
         await writer.drain()
 
         await reader.read()
 
-        writer.close()
+        if not qrexec:
+            writer.close()
 
         server.close()
 
@@ -179,5 +196,119 @@ class TestPolicyDaemon:
                b'just_evaluate=no\n\n'
 
         await self.send_data(async_server, tmp_path, data)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_simple_qrexec_request(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple+d=a ignore ignore ignore\0b\0\c'
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_called_once_with(
+            domain_id='dummy_id', source='b', intended_target='c',
+            service_and_arg='d', process_ident='0', log=unittest.mock.ANY,
+            assume_yes_for_ask=True, just_evaluate=True,
+            policy_cache=unittest.mock.ANY)
+
+    @pytest.mark.asyncio
+    async def test_unfinished_request(
+            self, mock_request, qrexec_server, tmp_path):
+
+        data = b'unfinished'
+
+        task = self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_too_long_qrexec_request(
+            self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple+' + b'a' * 65536 + b' ignore ignore ignore\0a\0b'
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_qrexec_request_too_long_source_qube_name(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple+a b c d\0' + b'c' * 32 + '\0d'
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_qrexec_request_too_long_destination_qube_name(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple+a b c d\0d\0' + b'c' * 32
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_qrexec_request_no_nul_separator(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple+a b c d\0' + b'c' * 31
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_qrexec_request_empty_argument(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple+ b c d\0e\0f'
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_qrexec_request_no_argument(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple b c d\0e\0f'
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_qrexec_request_wrong_service(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.WrongServiceName+a b c d\0e\0f'
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_qrexec_request_bad_source_name(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple+a b c d\0\n\0f'
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_qrexec_request_bad_destination_name(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple+a b c d\0e\0\n'
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
+
+        mock_request.assert_not_called()
+
+    @pytest.mark.asyncio
+    async def test_qrexec_request_two_nul_chars(self, mock_request, qrexec_server, tmp_path):
+
+        data = b'policy.EvalSimple+a b c d\0e\0\0'
+
+        await self.send_data(qrexec_server, tmp_path, data, True)
 
         mock_request.assert_not_called()
