QubesOS/qubes-core-qrexec

commit 6daa16d6b079070d69754ac9cfa0ed3cd77a3d85

Prevent log injection into qrexec-agent
A client must not send MSG_DATA_STDOUT, MSG_DATA_STDERR, or
MSG_DATA_EXIT_CODE, and a server must not send MSG_DATA_STDIN.  Check
for this to prevent a client from being able to cause the server to
write arbitrary unsanitized data to stderr, which will then be logged on
the server.

Modified on commit: removed dead code regarding replace_chars_stderr

libqrexec/remote.c
@@ -34,7 +34,7 @@
 int handle_remote_data(
     libvchan_t *data_vchan, int stdin_fd, int *status,
     struct buffer *stdin_buf, int data_protocol_version,
-    bool replace_chars_stdout, bool replace_chars_stderr)
+    bool replace_chars_stdout, bool replace_chars_stderr, bool is_service)
 {
     struct msg_header hdr;
     const size_t max_len = max_data_chunk_size(data_protocol_version);
@@ -74,6 +74,11 @@ int handle_remote_data(
              * of VM-VM connection */
             case MSG_DATA_STDIN:
             case MSG_DATA_STDOUT:
+                if (hdr.type != (is_service ? MSG_DATA_STDIN : MSG_DATA_STDOUT)) {
+                    LOG(ERROR, is_service ? "client sent MSG_DATA_STDOUT" : "service sent MSG_DATA_STDIN");
+                    continue;
+                }
+
                 if (stdin_fd < 0)
                     /* discard the data */
                     continue;
@@ -99,6 +104,11 @@ int handle_remote_data(
                 }
                 break;
             case MSG_DATA_STDERR:
+                if (is_service) {
+                    LOG(ERROR, "client sent MSG_DATA_STDERR");
+                    continue;
+                }
+
                 if (replace_chars_stderr)
                     do_replace_chars(buf, hdr.len);
                 /* stderr of remote service, log locally */
@@ -108,6 +118,11 @@ int handle_remote_data(
                 }
                 break;
             case MSG_DATA_EXIT_CODE:
+                if (is_service) {
+                    LOG(ERROR, "client sent MSG_DATA_EXIT_CODE");
+                    continue;
+                }
+
                 /* remote process exited, so there is no sense to send any data
                  * to it */
                 if (hdr.len < sizeof(*status)) {

libqrexec/libqrexec-utils.h
@@ -191,7 +191,7 @@ void do_replace_chars(char *buf, int len);
 int handle_remote_data(
     libvchan_t *data_vchan, int stdin_fd, int *status,
     struct buffer *stdin_buf, int data_protocol_version,
-    bool replace_chars_stdout, bool replace_chars_stderr);
+    bool replace_chars_stdout, bool replace_chars_stderr, bool is_service);
 
 /*
  * Handle data from the specified FD (cannot be -1) and send it over vchan

libqrexec/process_io.c
@@ -274,7 +274,8 @@ int process_io(const struct process_io_request *req) {
                     stdin_buf,
                     data_protocol_version,
                     replace_chars_stdout > 0,
-                    replace_chars_stderr > 0)) {
+                    replace_chars_stderr > 0,
+                    is_service)) {
             case REMOTE_ERROR:
                 handle_vchan_error("read");
                 break;

commit da13102ee72f47c7f1a67f51a4d6805956bfad10

Relax validation of MSG_DATA_STDIN/MSG_DATA_STDOUT
Since they are handled the same way, do not ignore remote side sending
the wrong one, just warn about it (once). QWT have it mixed up, so
ignoring such messages breaks communication with QWT.

Fixes QubesOS/qubes-issues#7917
Fixes: 6daa16d "Prevent log injection into qrexec-agent"

libqrexec/remote.c
@@ -40,6 +40,7 @@ int handle_remote_data(
     const size_t max_len = max_data_chunk_size(data_protocol_version);
     char *buf;
     int rc = REMOTE_ERROR;
+    bool msg_data_warned = false;
 
     /* do not receive any data if we have something already buffered */
     switch (flush_client_data(stdin_fd, stdin_buf)) {
@@ -74,9 +75,10 @@ int handle_remote_data(
              * of VM-VM connection */
             case MSG_DATA_STDIN:
             case MSG_DATA_STDOUT:
-                if (hdr.type != (is_service ? MSG_DATA_STDIN : MSG_DATA_STDOUT)) {
+                if (hdr.type != (is_service ? MSG_DATA_STDIN : MSG_DATA_STDOUT) &&
+                        !msg_data_warned) {
                     LOG(ERROR, is_service ? "client sent MSG_DATA_STDOUT" : "service sent MSG_DATA_STDIN");
-                    continue;
+                    msg_data_warned = true;
                 }
 
                 if (stdin_fd < 0)