65d8581b8e03c11344f3c4ec8bb667f6b15e645b
Demi Marie Obenour <demi@invisiblethingslab.com>
2023-03-31 14:57:25 -0400

    Check the return value of strdup()
    Avoids a NULL pointer dereference.

--- daemon/qrexec-daemon.c
+++ daemon/qrexec-daemon.c
@@ -1128,10 +1128,12 @@ int main(int argc, char **argv)
                 opt_quiet = 1;
                 break;
             case 'd' + 128:
-                socket_dir = strdup(optarg);
+                if ((socket_dir = strdup(optarg)) == NULL)
+                    err(1, "strdup()");
                 break;
             case 'p':
-                policy_program = strdup(optarg);
+                if ((policy_program = strdup(optarg)) == NULL)
+                    err(1, "strdup()");
                 break;
             case 'D':
                 opt_direct = 1;
