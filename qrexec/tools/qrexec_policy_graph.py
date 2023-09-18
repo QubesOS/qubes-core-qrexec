QubesOS/qubes-core-qrexec

commit 6e6226adafbdd54142dea18249af3c46f684fcb1

Add types to Python code
This adds lots of types to Python code, making it easier to understand
and maintain.  A mypy.ini is added to exclude the tests.

qrexec/tools/qrexec_policy_graph.py
@@ -105,16 +105,16 @@ def main(args=None):
     else:
         system_info = utils.get_system_info()
 
-    sources = list(system_info["domains"].keys())
+    sources = list(system_info.keys())
     if args.source:
         sources = args.source
 
-    targets = list(system_info["domains"].keys())
+    targets = list(system_info.keys())
     targets.append("@dispvm")
     targets.extend(
         "@dispvm:" + dom
-        for dom in system_info["domains"]
-        if system_info["domains"][dom]["template_for_dispvms"]
+        for dom in system_info
+        if system_info[dom]["template_for_dispvms"]
     )
 
     connections = set()

mypy.ini
@@ -1,0 +1,5 @@
+[mypy]
+exclude = (?x)(
+  # tests
+  ^qrexec/tests/
+ )

qrexec/exc.py
@@ -18,7 +18,9 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, see <https://www.gnu.org/licenses/>.
 #
-
+from __future__ import annotations
+import pathlib
+from typing import Optional
 
 class AccessDenied(Exception):
     """
@@ -29,15 +31,15 @@ class AccessDenied(Exception):
     (e.g. because we applied a policy that says `notify=no`).
     """
 
-    def __init__(self, *args, notify=True, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, msg: str, notify:bool=True):
+        super().__init__(msg)
         self.notify = notify
 
 
 class PolicySyntaxError(AccessDenied):
     """Syntax error in qrexec policy, abort parsing"""
 
-    def __init__(self, filepath, lineno, msg):
+    def __init__(self, filepath: Optional[pathlib.PurePath], lineno: Optional[int], msg: str):
         super().__init__(
             f"{filepath or '<unknown>'}:{lineno}: {msg}"
         )
@@ -46,14 +48,14 @@ class PolicySyntaxError(AccessDenied):
 class PolicyNotFound(AccessDenied):
     """Policy was not found for this service"""
 
-    def __init__(self, service_name):
+    def __init__(self, service_name: str):
         super().__init__("Policy not found for service {}".format(service_name))
 
 
 class QubesMgmtException(Exception):
     """Exception returned by qubesd"""
 
-    def __init__(self, exc_type):
+    def __init__(self, exc_type: str):
         super().__init__()
         self.exc_type = exc_type
 

qrexec/tests/cli.py
@@ -130,7 +130,7 @@ def system_info():
                 "template_for_dispvms": False,
                 "guivm": None,
             },
-        }
+        },
     }
     with mock.patch("qrexec.utils.get_system_info") as mock_system_info:
         mock_system_info.return_value = system_info

qrexec/tests/policy_parser.py
@@ -95,7 +95,7 @@ SYSTEM_INFO = {
             "template_for_dispvms": False,
             "power_state": "Halted",
         },
-    }
+    },
 }
 
 # a generic request helper

qrexec/tests/qrexec_policy_daemon.py
@@ -56,8 +56,8 @@ class TestPolicyDaemon:
                     "b": {"tags": ["guivm-c", "created-by-dom0"]},
                     "c": {"tags": ["created-by-dom0"]},
                     "dom0": {"tags": []},
-                }
-            }
+                },
+            },
         )
         monkeypatch.setattr(
             "qrexec.tools.qrexec_policy_daemon.get_system_info", mock_system

qrexec/tools/qrexec_policy_exec.py
@@ -18,12 +18,15 @@
 # License along with this library; if not, see <https://www.gnu.org/licenses/>.
 #
 
+from __future__ import annotations
 import argparse
 import logging
 import logging.handlers
 import pathlib
 import sys
 import asyncio
+import subprocess
+from typing import Optional, List, Union, Dict, Type
 
 from .. import DEFAULT_POLICY, POLICYPATH
 from .. import exc
@@ -38,31 +41,32 @@ def create_default_policy(service_name):
         policy.write(DEFAULT_POLICY)
 
 
-class JustEvaluateResult(Exception):
-    def __init__(self, exit_code):
+class JustEvaluateResult(BaseException):
+    exit_code: int
+    def __init__(self, exit_code: int):
         super().__init__()
         self.exit_code = exit_code
 
 
 class JustEvaluateAllowResolution(parser.AllowResolution):
-    async def execute(self, caller_ident):
+    async def execute(self, caller_ident: str) -> None:
         raise JustEvaluateResult(0)
 
 
 class JustEvaluateAskResolution(parser.AskResolution):
-    async def execute(self, caller_ident):
+    async def execute(self, caller_ident: str) -> None:
         raise JustEvaluateResult(1)
 
 
 class AssumeYesForAskResolution(parser.AskResolution):
-    async def execute(self, caller_ident):
+    async def execute(self, caller_ident: str) -> None:
         return await self.handle_user_response(
             True, self.request.target
         ).execute(caller_ident)
 
 
 class AgentAskResolution(parser.AskResolution):
-    async def execute(self, caller_ident):
+    async def execute(self, caller_ident: str) -> None:
         domains = self.request.system_info["domains"]
         guivm = domains[self.request.source]["guivm"]
         if not guivm:
@@ -122,7 +126,7 @@ class AgentAskResolution(parser.AskResolution):
 
 
 class NotifyAllowedResolution(parser.AllowResolution):
-    async def execute(self, caller_ident):
+    async def execute(self, caller_ident: str) -> None:
         try:
             guivm = self.request.system_info["domains"][self.request.source][
                 "guivm"
@@ -174,7 +178,7 @@ async def notify(guivm, params):
 
 
 class LogAllowedResolution(NotifyAllowedResolution):
-    async def execute(self, caller_ident):
+    async def execute(self, caller_ident: str) -> None:
         log_prefix = (
             "qrexec: {request.service}{request.argument}: "
             "{request.source} -> {request.target}:".format(request=self.request)
@@ -243,9 +247,8 @@ argparser.add_argument(
     help="Qrexec process identifier - for connecting data channel",
 )
 
-
-def main(args=None):
-    args = argparser.parse_args(args)
+def main(args: Optional[List[str]]) -> Union[str, int]:
+    parsed_args = argparser.parse_args(args)
 
     log = logging.getLogger("policy")
     log.setLevel(logging.INFO)
@@ -253,18 +256,18 @@ def main(args=None):
         handler = logging.handlers.SysLogHandler(address="/dev/log")
         log.addHandler(handler)
 
-    policy_cache = PolicyCache(args.path)
+    policy_cache = PolicyCache(parsed_args.path)
 
     return asyncio.run(
         handle_request(
-            args.domain_id,
-            args.source,
-            args.intended_target,
-            args.service_and_arg,
-            args.process_ident,
+            parsed_args.domain_id,
+            parsed_args.source,
+            parsed_args.intended_target,
+            parsed_args.service_and_arg,
+            parsed_args.process_ident,
             log,
-            just_evaluate=args.just_evaluate,
-            assume_yes_for_ask=args.assume_yes_for_ask,
+            just_evaluate=parsed_args.just_evaluate,
+            assume_yes_for_ask=parsed_args.assume_yes_for_ask,
             policy_cache=policy_cache,
         )
     )
@@ -272,15 +275,15 @@ def main(args=None):
 
 # pylint: disable=too-many-arguments,too-many-locals
 async def handle_request(
-    domain_id,
-    source,
-    intended_target,
-    service_and_arg,
-    process_ident,
+    domain_id: str,
+    source: str,
+    intended_target: str,
+    service_and_arg: str,
+    process_ident: str,
     log,
-    just_evaluate=False,
-    assume_yes_for_ask=False,
-    allow_resolution_type=None,
+    just_evaluate: bool = False,
+    assume_yes_for_ask: bool = False,
+    allow_resolution_type: Optional[type]=None,
     policy_cache=None,
     system_info=None,
 ):
@@ -306,6 +309,7 @@ async def handle_request(
         else:
             policy = parser.FilePolicy(policy_path=POLICYPATH)
 
+        allow_resolution_class: Type[parser.AllowResolution]
         if allow_resolution_type is None:
             allow_resolution_class = LogAllowedResolution
         else:

commit 02ff1aad680eefdeb48bbc9bc3afbfd7a448242a

Fix qrexec-policy-graph tool
The utils.get_system_info() still returns full system_info dict, not
just "domains" element (this change was abandoned in the final patch
version).

Fixes: 6e6226a "Add types to Python code"

qrexec/tools/qrexec_policy_graph.py
@@ -105,16 +105,16 @@ def main(args=None):
     else:
         system_info = utils.get_system_info()
 
-    sources = list(system_info.keys())
+    sources = list(system_info["domains"].keys())
     if args.source:
         sources = args.source
 
-    targets = list(system_info.keys())
+    targets = list(system_info["domains"].keys())
     targets.append("@dispvm")
     targets.extend(
         "@dispvm:" + dom
-        for dom in system_info
-        if system_info[dom]["template_for_dispvms"]
+        for dom in system_info["domains"]
+        if system_info["domains"][dom]["template_for_dispvms"]
     )
 
     connections = set()