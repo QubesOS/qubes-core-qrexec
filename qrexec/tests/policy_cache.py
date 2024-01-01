f2ed5fb57993f5c6d7a889ad3cb0c0c6b459fc4e
Demi Marie Obenour <demi@invisiblethingslab.com>
2023-12-31 21:58:02 -0500

    Support additional policy directories
    This allows multiple directories to contain qrexec policy, which allows
    for transient policy that disappears on reboot.
    
    Fixes: QubesOS/qubes-issues#8513

--- qrexec/tests/policy_cache.py
+++ qrexec/tests/policy_cache.py
@@ -23,11 +23,20 @@ import asyncio
 import pytest
 import unittest
 import unittest.mock
+import pathlib
 
 from ..policy import utils
 
 
 class TestPolicyCache:
+    @pytest.fixture
+    def tmp_paths(self, tmp_path: pathlib.Path) -> list[pathlib.Path]:
+        path1 = tmp_path / "path1"
+        path2 = tmp_path / "path2"
+        path1.mkdir()
+        path2.mkdir()
+        return [path1, path2]
+
     @pytest.fixture
     def mock_parser(self, monkeypatch):
         mock_parser = unittest.mock.Mock()
@@ -37,58 +46,60 @@ class TestPolicyCache:
         return mock_parser
 
     def test_00_policy_init(self, tmp_path, mock_parser):
-        cache = utils.PolicyCache(tmp_path)
-        mock_parser.assert_called_once_with(policy_path=tmp_path)
+        cache = utils.PolicyCache([tmp_path])
+        mock_parser.assert_called_once_with(policy_path=[tmp_path])
 
     @pytest.mark.asyncio
-    async def test_10_file_created(self, tmp_path, mock_parser):
-        cache = utils.PolicyCache(tmp_path)
-        cache.initialize_watcher()
+    async def test_10_file_created(self, tmp_paths, mock_parser):
+        for i in tmp_paths:
+            cache = utils.PolicyCache(tmp_paths)
+            cache.initialize_watcher()
 
-        assert not cache.outdated
+            assert not cache.outdated
 
-        file = tmp_path / "test"
-        file.write_text("test")
+            (i / "file").write_text("test")
 
-        await asyncio.sleep(1)
+            await asyncio.sleep(1)
 
-        assert cache.outdated
-
-    @pytest.mark.asyncio
-    async def test_11_file_changed(self, tmp_path, mock_parser):
-        file = tmp_path / "test"
-        file.write_text("test")
-
-        cache = utils.PolicyCache(tmp_path)
-        cache.initialize_watcher()
-
-        assert not cache.outdated
-
-        file.write_text("new_content")
-
-        await asyncio.sleep(1)
-
-        assert cache.outdated
+            assert cache.outdated
 
     @pytest.mark.asyncio
-    async def test_12_file_deleted(self, tmp_path, mock_parser):
-        file = tmp_path / "test"
-        file.write_text("test")
+    async def test_11_file_changed(self, tmp_paths, mock_parser):
+        for i in tmp_paths:
+            file = i / "test"
+            file.write_text("test")
 
-        cache = utils.PolicyCache(tmp_path)
-        cache.initialize_watcher()
+            cache = utils.PolicyCache(tmp_paths)
+            cache.initialize_watcher()
 
-        assert not cache.outdated
+            assert not cache.outdated
 
-        os.remove(file)
+            file.write_text("new_content")
 
-        await asyncio.sleep(1)
+            await asyncio.sleep(1)
 
-        assert cache.outdated
+            assert cache.outdated
 
     @pytest.mark.asyncio
-    async def test_13_no_change(self, tmp_path, mock_parser):
-        cache = utils.PolicyCache(tmp_path)
+    async def test_12_file_deleted(self, tmp_paths, mock_parser):
+        for i in tmp_paths:
+            file = i / "test"
+            file.write_text("test")
+
+            cache = utils.PolicyCache(tmp_paths)
+            cache.initialize_watcher()
+
+            assert not cache.outdated
+
+            os.remove(file)
+
+            await asyncio.sleep(1)
+
+            assert cache.outdated
+
+    @pytest.mark.asyncio
+    async def test_13_no_change(self, tmp_paths, mock_parser):
+        cache = utils.PolicyCache(tmp_paths)
         cache.initialize_watcher()
 
         assert not cache.outdated
@@ -101,10 +112,10 @@ class TestPolicyCache:
     async def test_14_policy_move(self, tmp_path, mock_parser):
         policy_path = tmp_path / "policy"
         policy_path.mkdir()
-        cache = utils.PolicyCache(policy_path)
+        cache = utils.PolicyCache([policy_path])
         cache.initialize_watcher()
 
-        mock_parser.assert_called_once_with(policy_path=policy_path)
+        mock_parser.assert_called_once_with(policy_path=[policy_path])
 
         assert not cache.outdated
 
@@ -135,27 +146,35 @@ class TestPolicyCache:
 
         cache.get_policy()
 
-        call = unittest.mock.call(policy_path=policy_path)
+        call = unittest.mock.call(policy_path=[policy_path])
         assert mock_parser.mock_calls == [call, call, call]
 
     @pytest.mark.asyncio
-    async def test_20_policy_updates(self, tmp_path, mock_parser):
-        cache = utils.PolicyCache(tmp_path)
+    async def test_20_policy_updates(self, tmp_paths, mock_parser):
+        cache = utils.PolicyCache(tmp_paths)
         cache.initialize_watcher()
+        count = 0
 
-        mock_parser.assert_called_once_with(policy_path=tmp_path)
+        for i in tmp_paths:
+            call = unittest.mock.call(policy_path=tmp_paths)
 
-        assert not cache.outdated
+            count += 2
+            assert mock_parser.mock_calls == [call] * (count - 1)
+            cache = utils.PolicyCache(tmp_paths)
+            cache.initialize_watcher()
 
-        file = tmp_path / "test"
-        file.write_text("test")
+            l = len(mock_parser.mock_calls)
+            assert mock_parser.mock_calls == [call] * l
 
-        await asyncio.sleep(1)
+            assert not cache.outdated
 
-        assert cache.outdated
+            file = i / "test"
+            file.write_text("test")
 
-        cache.get_policy()
+            await asyncio.sleep(1)
 
-        call = unittest.mock.call(policy_path=tmp_path)
+            assert cache.outdated
 
-        assert mock_parser.mock_calls == [call, call]
+            cache.get_policy()
+
+            assert mock_parser.mock_calls == [call] * (count + 1)
