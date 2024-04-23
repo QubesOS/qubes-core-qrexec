QubesOS/qubes-core-qrexec

commit 3d658cc82739a6cb8ff8366737003a4f56ecdd35

Use a pipe instead of signals to notify readiness
This avoids using async signal unsafe functions in a signal handler.  It
also reduces the total amount of code.

daemon/qrexec-daemon.c
@@ -120,7 +120,6 @@ static const char *policy_program = QREXEC_POLICY_PROGRAM;
 #  define UNUSED(x) UNUSED_ ## x
 #endif
 
-static volatile int children_count;
 static volatile int child_exited;
 static volatile int terminate_requested;
 
@@ -134,24 +133,6 @@ libvchan_t *vchan;
 static
 #endif
 int protocol_version;
-
-static void sigusr1_handler(int UNUSED(x))
-{
-    if (!opt_quiet)
-        LOG(INFO, "Connected to VM");
-    exit(0);
-}
-
-static void sigchld_parent_handler(int UNUSED(x))
-{
-    children_count--;
-    /* starting value is 0 so we see dead real qrexec-daemon as -1 */
-    if (children_count < 0) {
-        LOG(ERROR, "Connection to the VM failed");
-        exit(1);
-    }
-}
-
 
 static char *remote_domain_name;	// guess what
 static int remote_domain_id;
@@ -308,29 +289,41 @@ static void init(int xid)
             startup_timeout = MAX_STARTUP_TIME_DEFAULT;
     }
 
+    int pipes[2];
     if (!opt_direct) {
-        struct sigaction action = {
-            .sa_handler = sigusr1_handler,
-            .sa_flags = 0,
-        };
-        if (sigaction(SIGUSR1, &action, NULL))
-            err(1, "sigaction");
-        action.sa_handler = sigchld_parent_handler;
-        if (sigaction(SIGCHLD, &action, NULL))
-            err(1, "sigaction");
+        if (pipe2(pipes, O_CLOEXEC))
+            err(1, "pipe2()");
         switch (pid=fork()) {
             case -1:
                 PERROR("fork");
                 exit(1);
             case 0:
+                close(pipes[0]);
                 break;
             default:
                 if (getenv("QREXEC_STARTUP_NOWAIT"))
                     exit(0);
+                close(pipes[1]);
                 if (!opt_quiet)
                     LOG(ERROR, "Waiting for VM's qrexec agent.");
+                struct pollfd fds[1] = {{ .fd = pipes[0], .events = POLLIN | POLLHUP, .revents = 0 }};
                 for (i=0;i<startup_timeout;i++) {
-                    sleep(1);
+                    int res = poll(fds, 1, 1000);
+                    if (res < 0)
+                        err(1, "poll()");
+                    if (res) {
+                        char buf[1];
+                        ssize_t bytes = read(pipes[0], buf, sizeof buf);
+                        if (bytes < 0)
+                            err(1, "read()");
+                        if (bytes == 0) {
+                            LOG(ERROR, "Connection to the VM failed");
+                            exit(1);
+                        }
+                        if (!opt_quiet)
+                            LOG(INFO, "Connected to VM");
+                        exit(0);
+                    }
                     if (!opt_quiet)
                         fprintf(stderr, ".");
                     if (i==startup_timeout-1) {
@@ -419,12 +412,13 @@ static void init(int xid)
         err(1, "signal");
     if (sigaction(SIGCHLD, &sigchld_action, NULL))
         err(1, "sigaction");
-    if (signal(SIGUSR1, SIG_DFL) == SIG_ERR)
-        err(1, "signal");
     if (sigaction(SIGTERM, &sigterm_action, NULL))
         err(1, "sigaction");
-    if (!opt_direct)
-        kill(getppid(), SIGUSR1);   // let the parent know we are ready
+    if (!opt_direct) {
+        if (write(pipes[1], "", 1) != 1)
+            err(1, "write(pipe)");
+        close(pipes[1]);
+    }
 }
 
 static int send_client_hello(int fd)

commit 6d660511f0d722098b4c708f80f4fa26d61f53a4

Avoid writing to an uninitialized file descriptor
Found by clang-tidy.  No tests, but the fix is trivial.

Fixes: 3d658cc82739 ("Use a pipe instead of signals to notify readiness")

daemon/qrexec-daemon.c
@@ -110,7 +110,6 @@ static const char default_user_keyword[] = "DEFAULT:";
 #define default_user_keyword_len_without_colon (sizeof(default_user_keyword)-2)
 
 static int opt_quiet = 0;
-static int opt_direct = 0;
 
 static const char *policy_program = QREXEC_POLICY_PROGRAM;
 
@@ -268,7 +267,7 @@ static int handle_agent_hello(libvchan_t *ctrl, const char *domain_name)
 static void signal_handler(int sig);
 
 /* do the preparatory tasks, needed before entering the main event loop */
-static void init(int xid)
+static void init(int xid, bool opt_direct)
 {
     char qrexec_error_log_name[256];
     int logfd;
@@ -376,7 +375,7 @@ static void init(int xid)
     }
     if (qubes_wait_for_vchan_connection_with_timeout(
                 vchan, wait_fd, false, startup_timeout) < 0) {
-        if (write(pipes[1], "\1", 1)) {}
+        if (!opt_direct && write(pipes[1], "\1", 1)) {}
         LOG(ERROR, "qrexec connection timeout");
         exit(3);
     }
@@ -1570,6 +1569,7 @@ int main(int argc, char **argv)
 {
     int i, opt;
     sigset_t selectmask;
+    bool opt_direct = false;
 
     {
         int null_fd = open("/dev/null", O_RDONLY|O_NOCTTY);
@@ -1613,7 +1613,7 @@ int main(int argc, char **argv)
     remote_domain_name = argv[optind+1];
     if (argc - optind >= 3)
         default_user = argv[optind+2];
-    init(remote_domain_id);
+    init(remote_domain_id, opt_direct);
 
     sigemptyset(&selectmask);
     sigaddset(&selectmask, SIGCHLD);