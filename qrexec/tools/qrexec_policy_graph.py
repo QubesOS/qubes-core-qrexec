67707951f89aeb0fadb7acfd1f0bfe1911b064db
notmuchtohide <106391832+notmuchtohide@users.noreply.github.com>
2026-02-28 22:43:32 +0000

    qrexec-policy-graph: add qube colors
    Fixes QubesOS/qubes-issues#3007
    
    Code decisions:
        1. Node styling is not organized:
        Node styling is added as nodes are found instead of being grouped
        together. It should have no impact on the final graph and was
        considered as a potentially acceptable trade-off for
        implementation simplicity.
    
        2. qubesadmin vs. system_info:
        The already used system_info did not provide qube colors,
        therefore it was obtained from qubesadmin.
    
    Styling decisions:
    
        1. Qube color was added to node borders in case of a literal qube
        (one that doesn't start with "@"). On non-literal qubes, nodes
        are dotted because it's not necessarily an individual qube.
    
        2. The chosen style of coloring borders instead of solid filling
        made it easier to deal with the inner text (example: black text
        on black colored qubes).

--- qrexec/tools/qrexec_policy_graph.py
+++ qrexec/tools/qrexec_policy_graph.py
@@ -24,6 +24,8 @@ import pathlib
 
 import sys
 
+import qubesadmin
+
 from .. import POLICYPATH
 from .. import exc
 from .. import utils
@@ -78,8 +80,8 @@ argparser.add_argument(
 )
 
 
-def handle_single_action(args, action):
-    """Get single policy action and output (or not) a line to add"""
+def handle_single_action(args, action, app):
+    """Get single policy action and output (or not) lines to add"""
     if args.skip_labels:
         service = ""
     else:
@@ -91,19 +93,41 @@ def handle_single_action(args, action):
     if action.rule.action.target:
         target = action.rule.action.target
     if args.target and target not in args.target:
-        return ""
+        return []
+
+    lines = []
+
+    # create nodes for source and target, with colors
+    for node in [action.request.source, target]:
+        if node.startswith("@"):  # non-literal qubes
+            node_attributes = "style = dotted"
+        else:
+            node_color = app.domains[node].label.color.replace("0x", "#")
+            node_attributes = f'color = "{node_color}", penwidth = 5'
+        lines.append(f'  "{node}" [{node_attributes}];\n')
+
+    # create edges with services as labels
     if args.full_output:
         color = "orange" if isinstance(action, parser.AskResolution) else "red"
-        return (
+        lines.append(
             f'  "{action.request.source}" -> "{target}" '
             f'[label="{service} {action.rule.action}" color={color}];\n'
         )
+        return lines
+
     if isinstance(action, parser.AskResolution):
         if args.include_ask:
-            return f'  "{action.request.source}" -> "{target}" [label="{service}" color=orange];\n'
+            lines.append(
+                f'  "{action.request.source}" -> "{target}" [label="{service}" color=orange];\n'
+            )
+            return lines
     elif isinstance(action, parser.AllowResolution):
-        return f'  "{action.request.source}" -> "{target}" [label="{service}" color=red];\n'
-    return ""
+        lines.append(
+            f'  "{action.request.source}" -> "{target}" [label="{service}" color=red];\n'
+        )
+        return lines
+
+    return []
 
 
 def main(args=None, output=sys.stdout):
@@ -159,6 +183,8 @@ def main(args=None, output=sys.stdout):
     )
     targets.append("@default")
 
+    app = qubesadmin.Qubes()
+
     connections = set()
 
     policy = parser.FilePolicy(policy_path=args.policy_dir)
@@ -195,14 +221,15 @@ def main(args=None, output=sys.stdout):
                         system_info=system_info,
                     )
                     action = policy.evaluate(request)
-                    line = handle_single_action(args, action)
+                except exc.AccessDenied:
+                    continue
+
+                for line in handle_single_action(args, action, app):
                     if line in connections:
                         continue
                     if line:
                         output.write(line)
                     connections.add(line)
-                except exc.AccessDenied:
-                    continue
 
     output.write("}\n")
     if args.output:
